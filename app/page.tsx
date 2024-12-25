b6baf39ff8aa5192ee62f2445a8e4aebeb01696f
Doms <dlmagat08@proton.me>
2024-12-25 12:17:21 +0800

    fixed some styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image"
-
 import { movies } from "@/lib/types"
 const API_KEY = process.env.TMDB_API_KEY
 
@@ -20,17 +19,15 @@ export default async function Home() {
             <div className="grid grid-cols-2 gap-3 md:grid-cols-5">
                 {movieList.map((movie: movies) =>
                     <div key={movie.id} className="border border-black p-4 my-5">
-                        <div>
-                            <Image
-                                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-                                alt={movie.title}
-                                layout="responsive"
-                                width={300}
-                                height={300}
-                            />
-                            <h2 className="text-xl font-bold text-center">{movie.title}</h2>
-                            <p className="text-center">{movie.release_date}</p>
-                        </div>
+                        <Image
+                            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                            alt={movie.title}
+                            layout="responsive"
+                            width={300}
+                            height={300}
+                        />
+                        <h2 className="text-base md:text-xl font-bold text-center">{movie.title}</h2>
+                        <p className="text-center">{movie.release_date}</p>
                     </div>
                 )}
             </div>
