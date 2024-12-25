e6bf757c7e4dc601c450da6e870d176e72a8782b
Doms <dlmagat08@proton.me>
2024-12-25 17:21:27 +0800

    components

---
+++ components/PopularMovies.tsx
@@ -0,0 +1,37 @@
+import { API_KEY, movies } from '@/lib/types';
+import Image from 'next/image';
+import React from 'react'
+
+const PopularMovies = async () => {
+    const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`)
+
+    if (!response.ok) {
+        throw new Error("error fetching data");
+    }
+
+    const data = await response.json()
+    const movieList = data.results
+
+    console.log(movieList)
+    return (
+        <section>
+            <div className="grid grid-cols-2 gap-3 md:grid-cols-5">
+                {movieList.map((movie: movies) =>
+                    <div key={movie.id} className="border border-black p-4 my-5">
+                        <Image
+                            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                            alt={movie.title}
+                            layout="responsive"
+                            width={300}
+                            height={300}
+                        />
+                        <h2 className="text-base md:text-xl font-bold text-center">{movie.title}</h2>
+                        <p className="text-center">{movie.release_date}</p>
+                    </div>
+                )}
+            </div>
+        </section>
+    )
+}
+
+export default PopularMovies
