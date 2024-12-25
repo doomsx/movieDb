4f6d6a94d2b0c0193cf14bb3df20e15084ae4388
Doms <dlmagat08@proton.me>
2024-12-25 17:21:45 +0800

    new pages

---
+++ app/popular-movies/page.tsx
@@ -0,0 +1,11 @@
+import PopularMovies from '@/components/PopularMovies'
+import React from 'react'
+
+const Popular_Movies = () => {
+
+    return (
+        <PopularMovies />
+    )
+}
+
+export default Popular_Movies
