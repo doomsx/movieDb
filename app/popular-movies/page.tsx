ff637054a544cdb3f7264ec4c790cb11ab7ec511
Doms <dlmagat08@proton.me>
2025-04-23 20:08:16 +0800

    added title and margin top

--- app/popular-movies/page.tsx
+++ app/popular-movies/page.tsx
@@ -6,7 +6,8 @@ import React from 'react'
 const PopularMovies = async () => {
     const movieList = await popularMovies()
     return (
-        <section>
+        <section className='mt-10'>
+            <h1 className='text-center text-5xl font-bold'>Popular Movies</h1>
             <div className="grid grid-cols-2 gap-3 md:grid-cols-5">
                 {movieList.map((movie: movies) =>
                     <Card key={movie.id} movie={movie} />
