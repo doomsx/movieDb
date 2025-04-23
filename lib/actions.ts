f2590a5146eecadd2b8aea234078092280d2e142
Doms <dlmagat08@proton.me>
2025-04-23 20:07:46 +0800

    added new fetch endpoint

--- lib/actions.ts
+++ lib/actions.ts
@@ -10,3 +10,14 @@ export const popularMovies = async () => {
     const data = await response.json()
     return data.results
 }
+
+export const trendingMovies = async () => {
+    const response = await fetch(`https://api.themoviedb.org/3/trending/movie/week?api_key=${API_KEY}&language=en-US&page=1`)
+
+    if (!response.ok) {
+        throw new Error("Error fetching data");
+    }
+
+    const data = await response.json()
+    return data.results
+}
