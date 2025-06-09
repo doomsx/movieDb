c30ba4a3db0b80806dd7b047f502a868e5282fee
Dominique <dlmagat08@gmail.com>
2025-06-09 20:49:15 +0800

    navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,17 +2,26 @@ import Link from 'next/link'
 import React from 'react'
 
 const Navbar = () => {
+    const categories = [
+        { key: "popular", label: "Popular" },
+        { key: "trending", label: "Trending" },
+        { key: "top-rated", label: "Top Rated" },
+        { key: "upcoming", label: "Upcoming" },
+        { key: "now-playing", label: "Now Playing" },
+    ];
     return (
-        <div className='flex justify-around py-2'>
+        <nav className='flex justify-between p-2'>
             <div>
                 <Link href='/'>
                     <h1 className="text-xl ">MovieDb</h1>
                 </Link>
             </div>
             <div className="flex gap-3">
-                <Link href='/movies' className="underline">Movies</Link>
+                {categories.map((cat) => (
+                    <Link href={`/movies/${cat.key}`} key={cat.key} className="underline">{cat.label}</Link>
+                ))}
             </div>
-        </div>
+        </nav>
     )
 }
 
