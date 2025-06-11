74afbe6e848e6363b4a03fd64a67744228982ccb
Dominique <dlmagat08@gmail.com>
2025-06-11 19:59:49 +0800

    made the navbar responsive

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,19 @@
+"use client"
+import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
+import React, { useRef } from 'react'
+import burger from "@/public/burger.svg"
+import close from "@/public/close.svg"
 
 const Navbar = () => {
+    const nav = useRef<HTMLHtmlElement | null>(null)
+
+    const openNav = () => {
+        if (nav.current) {
+            nav.current.classList.toggle("hidden");
+        }
+    };
+
     const categories = [
         { key: "popular", label: "Popular" },
         { key: "trending", label: "Trending" },
@@ -9,19 +21,43 @@ const Navbar = () => {
         { key: "upcoming", label: "Upcoming" },
         { key: "now-playing", label: "Now Playing" },
     ];
+
     return (
-        <nav className='flex justify-between p-2'>
-            <div>
-                <Link href='/'>
-                    <h1 className="text-xl ">MovieDb</h1>
-                </Link>
-            </div>
-            <div className="flex gap-3">
-                {categories.map((cat) => (
-                    <Link href={`/movies/${cat.key}`} key={cat.key} className="underline">{cat.label}</Link>
-                ))}
-            </div>
-        </nav>
+        <header className='bg-gray-800 flex justify-between items-center px-2 py-3'>
+            <Link href='/'>
+                <h1 className="text-xl text-white">MovieDb</h1>
+            </Link>
+
+            <nav ref={nav} className="hidden bg-gray-800 fixed top-0 right-0 z-50 h-screen w-3/4 md:relative md:h-auto md:w-auto md:flex md:flex-row gap-3">
+                <Image
+                    src={close}
+                    onClick={openNav}
+                    alt=''
+                    width={40}
+                    height={40}
+                    className='block right-0 fixed px-2 py-3 md:hidden'
+                />
+                <ul className='flex flex-col gap-5 mt-10 md:flex-row md:mt-0'>
+                    {categories.map((cat) => (
+                        <li key={cat.key}>
+                            <Link
+                                href={`/movies/${cat.key}`}
+                                className="text-white">{cat.label}
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
+
+            <Image
+                src={burger}
+                alt=""
+                onClick={openNav}
+                width={40}
+                height={40}
+                className='block md:hidden'
+            />
+        </header>
     )
 }
 
